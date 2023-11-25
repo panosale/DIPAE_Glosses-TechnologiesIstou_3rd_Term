dbe4f11f3d5d3d1e3f6475832a862877155e527e
panosale <panosalevr@hotmail.gr>
2023-11-25 19:46:07 +0200

    .

--- Ergasia2023/globalBehaviour.js
+++ Ergasia2023/globalBehaviour.js
@@ -1,4 +1,28 @@
-function copyText1(){
-    let text1text = document.getElementById("originalTextBox").value;
-    document.getElementById("copiedTextBox").value = text1text.toUpperCase();
+function messageWriting(labelName, messageFieldName){
+  if (document.getElementById(messageFieldName.id).value == "")
+    document.getElementById(labelName.id).style.color = "red";
+  else {
+    document.getElementById(labelName.id).style.color = "black";
   }
+  document.getElementById("lbl_messageLength").innerText = messageFieldName.value.length;
+}
+function btn_submit(inputFieldName) {
+  alert("*** ΜΗΝΥΜΑ ΠΡΟΣΩΜΕΙΩΣΗΣ ***\n\nΤο μήνυμα σας στάλθηκε με επιτυχία στη διεύθυνση: " + document.getElementById(inputFieldName.id).value);
+//  alert("*** ΜΗΝΥΜΑ ΠΡΟΣΩΜΕΙΩΣΗΣ ***\n\nΤο μήνυμα σας στάλθηκε με επιτυχία στη διεύθυνση: " + document.getElementById("inp_email").value);
+}
+function checkRequiredField(labelName, inputFieldName) {
+  switch (labelName.id) {
+    case ("lbl_name"):
+      if (document.getElementById(inputFieldName.id).value == "")
+        document.getElementById(labelName.id).style.color = "red";
+      else
+        document.getElementById(labelName.id).style.color = "black";
+      break;
+    case ("lbl_email"):
+      if (document.getElementById(inputFieldName.id).value == "")
+        document.getElementById(labelName.id).style.color = "red";
+      else
+        document.getElementById(labelName.id).style.color = "black";
+      break;
+  }
+}
