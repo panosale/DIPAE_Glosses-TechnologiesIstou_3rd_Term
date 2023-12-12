396ca1cf94d3b0ad1bf6ec741c4660520fc8015f
panosale <panosalevr@hotmail.gr>
2023-12-12 23:29:57 +0200

    .

--- Ergasia2023/globalBehaviour.js
+++ Ergasia2023/globalBehaviour.js
@@ -1,26 +1,47 @@
-function checkRequiredField(labelName, inputFieldName) {
+function checkRequiredField(labelName, inputFieldName) { // ΜΑΛΛΟΝ ΔΕΝ ΧΡΕΙΑΖΕΤΑΙ
 //  alert(document.getElementById(inputFieldName.id).ariaLabel.value);
       // if (document.getElementById(inputFieldName.id.label).value == "")
       //   document.getElementById(inputFieldName.id.label).style.color = "red";
       // else
       //   document.getElementById(inputFieldName.id.label).style.color = "black";
 
-  switch (labelName.id) {
-    case ("lbl_name"):
-      if (document.getElementById(inputFieldName.id).value == "")
-        document.getElementById(labelName.id).style.color = "red";
-      else
-        document.getElementById(labelName.id).style.color = "black";
-      break;
-    case ("lbl_email"):
-      if (document.getElementById(inputFieldName.id).value == "")
-        document.getElementById(labelName.id).style.color = "red";
-      else
-        document.getElementById(labelName.id).style.color = "black";
-      break;
-  }
+  // switch (labelName.id) {
+  //   case ("lbl_name"):
+  //     if (document.getElementById(inputFieldName.id).value == "")
+  //       document.getElementById(labelName.id).style.color = "red";
+  //     else
+  //       document.getElementById(labelName.id).style.color = "black";
+  //     break;
+  //   case ("lbl_email"):
+  //     if (document.getElementById(inputFieldName.id).value == "")
+  //       document.getElementById(labelName.id).style.color = "red";
+  //     else
+  //       document.getElementById(labelName.id).style.color = "black";
+  //     break;
+  // }
+}
+// ΓΙΑ ΤΗ ΔΙΑΧΕΙΡΙΣΗ ΤΩΝ ΦΩΤΟΓΡΑΦΙΩΝ ΣΤΗΝ ΕΝΟΤΗΤΑ ΧΟΜΠΙ & ΔΡΑΣΤΗΡΙΟΤΗΤΕΣ
+var currentPos = 0;
+const imagesPath = "images\\";
+const picsArray = new Array("PANO_20230418_174909.jpg", "PANO_20230419_194026.jpg", "PANO_20230419_201414.jpg", "Dsc00939.jpg", "IMG_4452.JPG", "IMG_1662.JPG", "IMG_0695.JPG", "MpleTzami.jpg", "IMG_9563.JPG", "IMG_9537.JPG");
+function previousImage() { 
+  if (currentPos > 0)
+    currentPos--;
+  else
+    currentPos = picsArray.length - 1;
+  document.getElementById("img_myImages").src = imagesPath + picsArray[currentPos];
+}
+function nextImage() {
+  if (currentPos < picsArray.length-1)
+    currentPos++;
+  else
+    currentPos = 0;
+  document.getElementById("img_myImages").src = imagesPath + picsArray[currentPos];
 }
 
+function clearAllElementsFromMainElement(elmnt) {
+  elmnt.innerHTML = "";
+}
 function messageWriting(labelName, messageFieldName){
   if (document.getElementById(messageFieldName.id).value == "")
     document.getElementById(labelName.id).style.color = "red";
@@ -30,14 +51,18 @@ function messageWriting(labelName, messageFieldName){
   document.getElementById("lbl_messageLength").innerText = messageFieldName.value.length + "/100";
 }
 function messageSent() { // ΓΙΑ ΔΟΚΙΜΕΣ ΚΑΙ ΔΙΑΓΡΑΦΗ
-  var tmpDiv = document.getElementById("fieldsArea");
-  tmpDiv.style.display = "none";
+  // const contactDivElements = document.getElementById("fieldsArea").children;
+  // alert(contactDivElements[4].className);
+  var originalState = $("#fieldsArea").html();
+  clearAllElementsFromMainElement(document.getElementById("fieldsArea"));
   setTimeout(() => {
+    
     if (tmpDiv.style.display === "none")
       tmpDiv.style.display = "block";
     else
       tmpDiv.style.display = "none";
     }, 2000);
+    $("#fieldsArea").html(originalState);
 }
 function btn_submit() {
   messageSent();
