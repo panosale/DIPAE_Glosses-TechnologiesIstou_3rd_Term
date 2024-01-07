642c2701a3801c46e6b63bd1e254cf65292bb2e6
panosale <panosalevr@hotmail.gr>
2024-01-07 15:23:09 +0200

    .

--- globalBehaviour.js
+++ globalBehaviour.js
@@ -21,15 +21,34 @@ function nextImage() {
 var slideshowCurrentPos = 0;
 var slideshowInterval;
 const concertImagesPath = "images\\concerts\\";
-const slideshowPicsArray = new Array("2008-06-15_Knopfler_IMG_0057.JPG", "PANO_20230419_194026.jpg", "PANO_20230419_201414.jpg", "Dsc00939.jpg", "IMG_4452.JPG", "IMG_1662.JPG", "IMG_0695.JPG", "MpleTzami.JPG", "IMG_9563.JPG", "IMG_9537.JPG");
+const slideshowPicsArray = new Array(["2008-06-06_Papakonstantinou-Maxairitsas_IMG_0052.jpg", "6/6/2008 Βασίλης Παπακωνσταντίνου & Λαυρέντης Μαχαιρίτσας στο ΦΙΞ"],
+    ["2008-06-06_Zouganelis-Boulas_IMG_0045.jpg", "6/6/2008 Γιάννης Ζουγανέλης & Σάκης Μπουλάς στο ΦΙΞ"],
+    ["2008-06-15_Knopfler_IMG_0057.JPG", "15/6/2008 Mark Knopfler στον Λυκαβητό"], ["2008-06-15_Knopfler_IMG_0110.JPG", "15/6/2008 Mark Knopfler στον Λυκαβητό"],
+    ["2008-11-14_Arvanitaki&Galani_IMG_0736.jpg", "14/11/2008 Ελευθερία Αρβανιτάκη & Δήμητρα Γαλάνη στον ΦΙΞ"],
+    ["2009-03-28_Pavlidis_IMG_1350.JPG", "28/3/2009 Παύλος Παυλίδης στον Μύλο"], ["2009-03-28_Pavlidis_IMG_1354.JPG", "28/3/2009 Παύλος Παυλίδης στον Μύλο"],
+    ["2009-05-28_ACDC_IMG_1499.JPG", "28/5/2009 ACDC στο ΟΑΚΑ"], ["2009-05-28_ACDC_IMG_1530.JPG", "28/5/2009 ACDC στο ΟΑΚΑ"], ["2009-05-28_ACDC_IMG_1568.JPG", "28/5/2009 ACDC στο ΟΑΚΑ"], ["2009-05-28_ACDC_IMG_1577.JPG", "28/5/2009 ACDC στο ΟΑΚΑ"],
+    ["2009-05-28_ACDC_IMG_1617.JPG", "28/5/2009 ACDC στο ΟΑΚΑ"],
+    ["2009-07-08_Santana_IMG_2048.JPG", "8/7/2009 Carlos Santana στο ΟΑΚΑ"], ["2009-07-08_Santana_IMG_2054.JPG", "8/7/2009 Carlos Santana στο ΟΑΚΑ"], ["2009-07-08_Santana_IMG_2070.JPG", "8/7/2009 Carlos Santana στο ΟΑΚΑ"],
+    ["2009-07-08_Santana_IMG_2074.JPG", "8/7/2009 Carlos Santana στο ΟΑΚΑ"], ["2009-07-08_Santana_IMG_2088.JPG", "8/7/2009 Carlos Santana στο ΟΑΚΑ"],
+    ["2009-10-24_ZZTop_IMG_2850.jpg", "24/10/2009 ZZTop στο Κλειστό TaeKwonDo του Φαλήρου"], ["2009-10-24_ZZTop_IMG_2853.jpg", "24/10/2009 ZZTop στο Κλειστό TaeKwonDo του Φαλήρου"],
+    ["2010-09-03_U2_IMG_4073.jpg", "3/9/2010 U2 στο ΟΑΚΑ"], ["2010-09-03_U2_IMG_4077.jpg", "3/9/2010 U2 στο ΟΑΚΑ"], ["2010-09-03_U2_IMG_4094.jpg", "3/9/2010 U2 στο ΟΑΚΑ"],
+    ["2011-03-17_Manowar_IMG_7124.jpg", "17/3/2011 Manowar στο Principal"], ["2011-03-17_Manowar_IMG_7127.jpg", "17/3/2011 Manowar στο Principal"],
+    ["2016-07-05_Malamas_DSC_0962.jpg", "5/7/2016 Σωκράτης Μάλαμας στο ΦΙΞ"],
+    ["2019-06-08_James&IggyPop_IMG_20190608_210641.jpg", "8/6/2019 James στην Πλατεία Νερού"], ["2019-06-08_James&IggyPop_IMG_20190608_215840.jpg", "8/6/2019 James στην Πλατεία Νερού"],
+    ["2019-06-08_James&IggyPop_IMG_20190608_231038.jpg", "8/6/2019 IggyPop στην Πλατεία Νερού"], ["2019-06-08_James&IggyPop_IMG_20190609_003004.jpg", "8/6/2019 IggyPop στην Πλατεία Νερού"],
+    ["2022-07-16_IronMaiden_IMG_20220716_215231.jpg", "16/7/2022 Iron Maiden στο ΟΑΚΑ"], ["2022-07-16_IronMaiden_IMG_20220716_215810.jpg", "16/7/2022 Iron Maiden στο ΟΑΚΑ"],
+    ["2023-07-22_Guns'n'Roses_IMG_20230722_212714.jpg", "22/7/2023 Guns n'Roses στο ΟΑΚΑ"], ["2023-07-22_Guns'n'Roses_IMG_20230722_212856.jpg", "22/7/2023 Guns n'Roses στο ΟΑΚΑ"],
+    ["2023-07-22_Guns'n'Roses_IMG_20230722_224121_1.jpg", "22/7/2023 Guns n'Roses στο ΟΑΚΑ"], ["2023-07-22_Guns'n'Roses_IMG_20230722_233058_1.jpg", "22/7/2023 Guns n'Roses στο ΟΑΚΑ"]);
 function slideShow() {
   slideshowCurrentPos++;
   if (slideshowCurrentPos == slideshowPicsArray.length)
     slideshowCurrentPos = 0;
-  document.getElementById("img_currentImage").src = concertImagesPath + slideshowPicsArray[slideshowCurrentPos];
+  document.getElementById("img_currentImage").src = concertImagesPath + slideshowPicsArray[slideshowCurrentPos][0];
+  document.getElementById("span_currentImageInfo").innerHTML = slideshowPicsArray[slideshowCurrentPos][1];
+  // console.log(slideshowPicsArray[slideshowCurrentPos][1]);
 }
 function slideshowStart() {
-  slideshowInterval = setInterval(slideShow, 1000);
+  slideshowInterval = setInterval(slideShow, 2000);
 }
 function slideshowStop() {
   clearInterval(slideshowInterval);
