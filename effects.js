c2a61c31f0f37f2636215cfa964aaab5b2222e09
panosale <panosalevr@hotmail.gr>
2024-01-07 23:09:33 +0200

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,3 +1,36 @@
+var barInterval;
+var msecondsBarRan = 0;
+// var innerWidth = parseInt(document.getElementById("div_Inner").style.width);
+// Το παραπάνω γίνεται και έτσι:
+//var innerWidth = parseInt(div_Inner.style.width);
+function updateSeconds() {
+    msecondsBarRan++;
+    // document.getElementById("span_secondsBarRan").innerHTML = msecondsBarRan;
+    // Το παραπάνω γίνεται και έτσι:
+    //span_secondsBarRan.innerHTML = msecondsBarRan;
+}
+function progressStarted() {
+    if (innerWidth > 0)
+        document.getElementById("div_Inner").style.width = innerWidth-- + "px";
+        // Το παραπάνω γίνεται και έτσι:
+    //div_Inner.style.width = innerWidth-- + "px";
+    else
+        progressReset();
+}
+function runBar() {
+    progressReset();
+    var totalSecondsToBarRun = 4000; // 4 Seconds
+    barInterval = setInterval(progressStarted, totalSecondsToBarRun);
+}
+function progressReset() {
+    clearInterval(barInterval);
+    msecondsBarRan = 0;
+    document.getElementById("div_Outer").style.width = inp_barWidth.value + "px";
+    innerWidth = parseInt(document.getElementById("div_Outer").style.width) - 1;
+    document.getElementById("div_Inner").style.width = innerWidth + "px";
+    document.getElementById("div_Inner").style.backroundcolor= "darkgreen"; // Έλεγχος αν χρειάζεται αυτή η γραμμή
+}
+
 // ΔΕΝ ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ - ΑΡΧΗ
 // var id = null;
 // function myMove() {
